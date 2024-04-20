01bd68e9cf13e5965f2f395bfc840649629b73e7
Neel <neeljadhavforcoc@gmail.com>
2024-04-20 14:55:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Variable to track if the loading animation has been shown
+var loadingAnimationShown = false;
+
 // Function to check if the page is being loaded from the cache
 function isPageLoadedFromCache(event) {
     return event.persisted;
@@ -9,17 +12,26 @@ window.addEventListener('pageshow', function(event) {
         setTimeout(function() {
             hideLoading();
         }, 2700); // Adjusted delay to match the loading GIF duration
+    } else {
+        // If the page is loaded from cache, hide the loading GIF immediately
+        hideLoading();
     }
 });
 
 // Function to show loading GIF for 3 seconds before redirecting
 function showLoadingAndRedirect(loadingGifPath, redirectUrl) {
-    changeLoadingGif(loadingGifPath); // Change loading GIF source
-    showLoading(); // Show loading GIF
-    // Wait for the loading GIF to be shown and then redirect after 3 seconds
-    setTimeout(function() {
-        window.location.href = redirectUrl; // Redirect to the linked page after 3 seconds
-    }, 3000); // Adjusted delay to match the loading GIF duration
+    if (!loadingAnimationShown) {
+        changeLoadingGif(loadingGifPath); // Change loading GIF source
+        showLoading(); // Show loading GIF
+        loadingAnimationShown = true; // Update the flag to indicate that the loading animation has been shown
+        // Wait for the loading GIF to be shown and then redirect after 3 seconds
+        setTimeout(function() {
+            window.location.href = redirectUrl; // Redirect to the linked page after 3 seconds
+        }, 3000); // Adjusted delay to match the loading GIF duration
+    } else {
+        // If the loading animation has already been shown, redirect immediately
+        window.location.href = redirectUrl;
+    }
 }
 
 // Function to change loading GIF source
