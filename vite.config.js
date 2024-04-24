451bd52a04057da11b222987b412b3cfca61290d
Chaitanya_Yeranagula <chaitanya.yeranagula@finmkt.io>
2024-04-24 23:17:18 +0530

    fix ci

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: "/page/",
+  plugins: [],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    alias: [
+      
+    ]
+  },
 })
