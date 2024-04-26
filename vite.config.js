2e09d4c1703119fe48d329cedc80a6fd84a5544e
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2024-04-26 11:08:12 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/page/" : "/",
+// };
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/page/",
-  plugins: [],
+  plugins: [
+    vue(),
+  ],
   resolve: {
-    alias: [
-      
-    ]
+    alias: {
+      '@': fileURLToPath(new URL('src', import.meta.url))
+    }
   },
+  // server: {
+  //   port: '8080'
+  // },
+  // publicPath: process.env.NODE_ENV === "production" ? "/page/" : "/",
+
+
 })
